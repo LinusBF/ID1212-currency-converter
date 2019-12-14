e4c4d5ec8d6c4a45a310e201cce7b16172b0c6ff
LinusBF <contact@linusbf.com>
2019-12-14 21:12:39 +0100

    Made the repo calls transactional

--- src/main/java/com/linusbf/currcon/db/CurrencyRepository.java
+++ src/main/java/com/linusbf/currcon/db/CurrencyRepository.java
@@ -1,9 +1,13 @@
 package com.linusbf.currcon.db;
 import com.linusbf.currcon.model.Currency;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 public interface CurrencyRepository extends CrudRepository<Currency, Long> {
+    @Transactional
     List<Currency> findByName(String name);
+    @Transactional
     Currency findById(long id);
 }
